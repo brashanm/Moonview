bcde830c471264997c7c442cfe1bd693e9394f6c
Brashan <brashanm@gmail.com>
2023-11-26 15:04:24 -0500

    Initial Commit

---
+++ Moonview/MoonviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonviewApp.swift
+//  Moonview
+//
+//  Created by Brashan Mohanakumar on 2023-11-26.
+//
+
+import SwiftUI
+
+@main
+struct MoonviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
